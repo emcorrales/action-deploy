93c885d7d95abfbbea44d29259fca492f6a5b599
Emmanuel Corrales <contact@emcorrales.com>
2019-09-09 23:39:17 -0700

    Create an entrypoint script.

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+bundle install
+bundle exec jekyll build
+
+git config user.email $INPUT_EMAIL
+git config user.name $INPUT_NAME
+
+git add _site
+git commit -m "Deploy!"
+git subtree split -P _site -b gh-pages
+git push https://$INPUT_ACTOR:$INPUT_TOKEN@$INPUT_URL gh-pages --force
