0eeb8599e6185314d6756e762ee936e8123a16a8
Emmanuel Corrales <contact@emcorrales.com>
2019-09-13 22:40:37 -0700

    Change the directory where CNAME will be stored.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ MY_JEKYLL_SITE=my_jekyll_site
 gem install bundler
 bundle install
 bundle exec jekyll build -d $MY_JEKYLL_SITE
-[ ! -z $INPUT_CUSTOM_DOMAIN ] && echo $INPUT_CUSTOM_DOMAIN > _site/CNAME
+[ ! -z $INPUT_CUSTOM_DOMAIN ] && echo $INPUT_CUSTOM_DOMAIN > $MY_JEKYLL_SITE/CNAME
 
 git config user.name "$INPUT_GIT_COMMITER_NAME"
 git config user.email $INPUT_GIT_COMMITTER_EMAIL
