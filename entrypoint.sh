29823aeea0b7fddb09ee06102a3d99aeb4385ad6
Emmanuel Corrales <contact@emcorrales.com>
2019-09-13 22:29:07 -0700

    Set jekyll build directory.
    Default build directory "_site" is usually included on most Jekyll
    project gitignore file and cannot be staged.
    
    Create a different jekyll directory that can be staged for the build.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,15 @@
 #! /bin/bash -xe
+MY_JEKYLL_SITE=my_jekyll_site
+
 gem install bundler
 bundle install
-bundle exec jekyll build
+bundle exec jekyll build -d $MY_JEKYLL_SITE
 [ ! -z $INPUT_CUSTOM_DOMAIN ] && echo $INPUT_CUSTOM_DOMAIN > _site/CNAME
 
 git config user.name "$INPUT_GIT_COMMITER_NAME"
 git config user.email $INPUT_GIT_COMMITTER_EMAIL
 
-git add _site
+git add $MY_JEKYLL_SITE
 git commit -m "Deploy!"
-git subtree split -P _site -b gh-pages
+git subtree split -P $MY_JEKYLL_SITE -b gh-pages
 git push https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git gh-pages --force
