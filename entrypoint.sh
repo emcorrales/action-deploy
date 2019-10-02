89bd789297483dcbab1203f52b3cfc7b38bde153
Emmanuel Corrales <contact@emcorrales.com>
2019-10-01 22:23:35 -0700

    Build site using production environment.

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ MY_JEKYLL_SITE=my_jekyll_site
 
 gem install bundler
 bundle install
-bundle exec jekyll build -d $MY_JEKYLL_SITE
+JEKYLL_ENV=production bundle exec jekyll build -d $MY_JEKYLL_SITE
 [ ! -z $INPUT_CUSTOM_DOMAIN ] && echo $INPUT_CUSTOM_DOMAIN > $MY_JEKYLL_SITE/CNAME
 
 git config user.name "$INPUT_GIT_COMMITTER_NAME"
