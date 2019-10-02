958add9ef54f7030f771e20fd7c360c99c0da044
Emmanuel Corrales <contact@emcorrales.com>
2019-10-01 23:34:15 -0700

    Set git config globally.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ git clone --bare --branch=gh-pages https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@git
 JEKYLL_ENV=production bundle exec jekyll build -d $MY_JEKYLL_SITE
 [ ! -z $INPUT_CUSTOM_DOMAIN ] && echo $INPUT_CUSTOM_DOMAIN > $MY_JEKYLL_SITE/CNAME
 
-git config user.name "$INPUT_GIT_COMMITTER_NAME"
-git config user.email $INPUT_GIT_COMMITTER_EMAIL
+git config --global user.name "$INPUT_GIT_COMMITTER_NAME"
+git config --global user.email $INPUT_GIT_COMMITTER_EMAIL
 
 git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE checkout
 git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE add $MY_JEKYLL_SITE
