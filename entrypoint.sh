6c9b8b2a1f20d667118f71977cd4b2e0bf305cc2
Emmanuel Corrales <contact@emcorrales.com>
2019-10-27 08:56:53 +0800

    Delete all contents of the work tree.
     Delete all contents of the work tree before generating the new jekyll site.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,12 +7,13 @@ bundle install
 git clone --bare --branch=gh-pages https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git gh-pages
 git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE checkout gh-pages
 
+rm -f $MY_JEKYLL_SITE/*
 JEKYLL_ENV=production bundle exec jekyll build -d $MY_JEKYLL_SITE
 [ ! -z $INPUT_CUSTOM_DOMAIN ] && echo $INPUT_CUSTOM_DOMAIN > $MY_JEKYLL_SITE/CNAME
 
 git config --global user.name "$INPUT_GIT_COMMITTER_NAME"
 git config --global user.email $INPUT_GIT_COMMITTER_EMAIL
 
-git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE add $MY_JEKYLL_SITE/*
+git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE add $MY_JEKYLL_SITE
 git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE commit -m "Deploy!"
 git --git-dir=gh-pages --work-tree=$MY_JEKYLL_SITE push https://$GITHUB_ACTOR:$INPUT_GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git gh-pages
